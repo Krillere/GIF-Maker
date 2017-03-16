59c363cd7f71f660e509bd6965ecc13c8e40199a
Krillere <Christian@iMakezAppz.com>
2017-03-16 23:40:57 +0100

    Now correctly sets loop count in exported gif
    Could not import gifs made using the app before

--- GIF/GIF/GIFHandler.swift
+++ GIF/GIF/GIFHandler.swift
@@ -12,14 +12,26 @@ import Cocoa
 class GIFHandler {
 
     // MARK: Loading gifs (Returns tuple with images, loop count and seconds/frame
-    static func loadGIF(with image: NSImage) -> (images: [NSImage], loops:Int, secondsPrFrame: Float) {
-        let errorReturn:(images: [NSImage], loops:Int, secondsPrFrame: Float) = (images: [], loops: 0, secondsPrFrame: 0.2)
+    static func loadGIF(with image: NSImage) -> (images: [NSImage?], loops:Int, secondsPrFrame: Float) {
+        let errorReturn:(images: [NSImage?], loops:Int, secondsPrFrame: Float) = (images: [nil], loops: 0, secondsPrFrame: 0.2)
         
-        // Fetch the number of frames, frame duration, and loop count from the .gif
-        guard let bitmapRep = image.representations[0] as? NSBitmapImageRep,
-              let frameCount = (bitmapRep.value(forProperty: NSImageFrameCount) as? NSNumber)?.intValue,
-              let frameDuration = (bitmapRep.value(forProperty: NSImageCurrentFrameDuration) as? NSNumber)?.floatValue,
-              let loopCount = (bitmapRep.value(forProperty: NSImageLoopCount) as? NSNumber)?.intValue else { return errorReturn }
+        // Attempt to fetch the number of frames, frame duration, and loop count from the .gif
+        guard let bitmapRep = image.representations[0] as? NSBitmapImageRep else {
+            print("Error loading bitmapRep")
+            return errorReturn
+        }
+        guard let frameCount = (bitmapRep.value(forProperty: NSImageFrameCount) as? NSNumber)?.intValue else {
+            print("Error loading frameCount")
+            return errorReturn
+        }
+        guard let frameDuration = (bitmapRep.value(forProperty: NSImageCurrentFrameDuration) as? NSNumber)?.floatValue else {
+            print("Error loading frameDuration")
+            return errorReturn
+        }
+        guard let loopCount = (bitmapRep.value(forProperty: NSImageLoopCount) as? NSNumber)?.intValue else {
+            print("Error loading loopCount")
+            return errorReturn
+        }
         
         var retImages:[NSImage] = []
         
@@ -61,18 +73,20 @@ class GIFHandler {
     
     // Creates NSData from given images
     static func createGIFData(with images: [NSImage], loops: Int = 0, secondsPrFrame: Float = 0.2) -> Data {
-        // Loop count and seconds pr. frame
-        let prep = NSDictionary(dictionary: [kCGImagePropertyGIFDictionary:NSDictionary(dictionary: [kCGImagePropertyGIFDelayTime: secondsPrFrame, kCGImagePropertyGIFLoopCount: loops])])
+        // Loop count and frame duration
+        let frameDurationDic = NSDictionary(dictionary: [kCGImagePropertyGIFDictionary:NSDictionary(dictionary: [kCGImagePropertyGIFDelayTime: secondsPrFrame])])
+        let loopCountDic = NSDictionary(dictionary: [kCGImagePropertyGIFDictionary:NSDictionary(dictionary: [kCGImagePropertyGIFLoopCount: loops])])
         
         // Destination (A data object)
         guard let dataObj = CFDataCreateMutable(nil, 0),
-            let dst = CGImageDestinationCreateWithData(dataObj, kUTTypeGIF, images.count, nil) else { fatalError("Can't create gif") }
-
+              let dst = CGImageDestinationCreateWithData(dataObj, kUTTypeGIF, images.count, nil) else { fatalError("Can't create gif") }
+        CGImageDestinationSetProperties(dst, loopCountDic as CFDictionary) // Set loop count on object
+        
         // Iterate given images and add these to destination
         for n in 0 ..< images.count {
             let anImage = images[n]
             if let imageRef = anImage.cgImage(forProposedRect: nil, context: nil, hints: nil) {
-                CGImageDestinationAddImage(dst, imageRef, prep as CFDictionary)
+                CGImageDestinationAddImage(dst, imageRef, frameDurationDic as CFDictionary)
             }
         }
         
