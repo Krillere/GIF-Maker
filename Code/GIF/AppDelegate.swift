7f045f820d2f791fba07f6418f8f94fff9893b79
Krillere <Christian@iMakezAppz.com>
2017-08-15 18:38:57 +0200

    iAP Implemented and working

--- Code/GIF/AppDelegate.swift
+++ Code/GIF/AppDelegate.swift
@@ -22,8 +22,14 @@ class AppDelegate: NSObject, NSApplicationDelegate {
                                                selector: #selector(AppDelegate.productsLoaded),
                                                name: IAPHelper.IAPLoadedNotificationName,
                                                object: nil)
-        
-//        loadProducts()
+        NotificationCenter.default.addObserver(self, selector: #selector(AppDelegate.handlePurchaseNotification(_:)),
+                                               name: NSNotification.Name(rawValue: IAPHelper.IAPHelperPurchaseNotification),
+                                               object: nil)
+        NotificationCenter.default.addObserver(self,
+                                               selector: #selector(AppDelegate.failedPurchase),
+                                               name: NSNotification.Name(rawValue: IAPHelper.IAPPurchaseFailed),
+                                               object: nil)
+        loadProducts()
     }
     
     func applicationWillTerminate(_ aNotification: Notification) {
@@ -37,19 +43,25 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     
     // MARK: Pro / watermark removal
     // Should this really be in AppDelegate?
-    func createPurchaseMenuItem() {
+    func createPurchaseMenu() {
         guard let menu = NSApplication.shared().mainMenu else { return }
         let newItem = NSMenuItem(title: "UnlockPro", action: nil, keyEquivalent: "")
-        let newMenu = NSMenu(title: "Unlock Pro")
+        let newMenu = NSMenu(title: "Remove watermarks")
         
-        let unlockItem = NSMenuItem(title: "Unlock Pro", action: #selector(AppDelegate.unlockProButtonClicked), keyEquivalent: "")
-        let unlockedItem = NSMenuItem(title: "Previously unlocked Pro", action: #selector(AppDelegate.unlockedButtonClicked), keyEquivalent: "")
+        let unlockItem = NSMenuItem(title: "Remove watermarks", action: #selector(AppDelegate.unlockProButtonClicked), keyEquivalent: "")
+        let unlockedItem = NSMenuItem(title: "Previously purchased", action: #selector(AppDelegate.unlockedButtonClicked), keyEquivalent: "")
         
         newMenu.addItem(unlockItem)
         newMenu.addItem(unlockedItem)
         
         newItem.submenu = newMenu
         menu.insertItem(newItem, at: menu.items.count-1)
+    }
+    
+    func removePurchaseMenu() {
+        guard let menu = NSApplication.shared().mainMenu,
+            let item = menu.item(withTitle: "UnlockPro") else { return }
+        menu.removeItem(item)
     }
     
     // Unlock button clicked
@@ -59,13 +71,18 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     
     // Previously unlocked button clicked
     func unlockedButtonClicked() {
-        if let window = NSApplication.shared().keyWindow {
-            let alert = FancyAlert()
-            alert.messageText = "Unlocking.."
-            alert.informativeText = "Attempting to unlock.."
-            alert.alertStyle = .critical
-            alert.addButton(withTitle: "OK")
+        
+        let alert = FancyAlert()
+        alert.messageText = "Unlocking.."
+        alert.informativeText = "Attempting to unlock. This might take a while."
+        alert.alertStyle = .critical
+        alert.addButton(withTitle: "OK")
+        
+        if let window = NSApplication.shared().mainWindow {
             alert.beginSheetModal(for: window, completionHandler: nil)
+        }
+        else {
+            alert.runModal()
         }
         
         Products.store.restorePurchases()
@@ -84,7 +101,60 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     
     func productsLoaded() {
         if !Products.store.isProductPurchased(Products.Pro) {
-            createPurchaseMenuItem()
+            createPurchaseMenu()
+        }
+    }
+    
+    // Purchase did not succeed
+    func failedPurchase() {
+        
+        if !Products.store.isProductPurchased(Products.Pro) {
+            let alert = FancyAlert()
+            alert.messageText = "An error occurred"
+            alert.informativeText = "An error occurred during purchase. If you are trying to restore the purchase, make sure that you purchased it previously."
+            alert.alertStyle = .critical
+            alert.addButton(withTitle: "OK")
+            
+            if let window = NSApplication.shared().mainWindow {
+                alert.beginSheetModal(for: window, completionHandler: nil)
+            }
+            else {
+                alert.runModal()
+            }
+        }
+    }
+    
+    func handlePurchaseNotification(_ notification: Notification) {
+        
+        if Products.store.isProductPurchased(Products.Pro) {
+            removePurchaseMenu()
+            
+            let alert = FancyAlert()
+            alert.messageText = "Watermarks removed"
+            alert.informativeText = "Watermarks are now removed!"
+            alert.alertStyle = .critical
+            alert.addButton(withTitle: "OK")
+            
+            if let window = NSApplication.shared().mainWindow {
+                alert.beginSheetModal(for: window, completionHandler: nil)
+            }
+            else {
+                alert.runModal()
+            }
+        }
+        else { // Error
+            let alert = FancyAlert()
+            alert.messageText = "An error occurred"
+            alert.informativeText = "An error occurred during purchase. If you are trying to restore the purchase, make sure that you purchased it previously."
+            alert.alertStyle = .critical
+            alert.addButton(withTitle: "OK")
+            
+            if let window = NSApplication.shared().mainWindow {
+                alert.beginSheetModal(for: window, completionHandler: nil)
+            }
+            else {
+                alert.runModal()
+            }
         }
     }
 }
